c3e74288027448ba171b8d953fdbe8eaf9abbe91
Valerii Pometun <112505741+Valerii2022@users.noreply.github.com>
2024-08-06 09:15:52 +0300

    update to react start project

--- src/image/logo.js
+++
@@ -1,5 +0,0 @@
-import mainLogo from './logo/logo.jpg';
-import welder from './logo/welder.png';
-import leftArrow from './logo/left.png';
-import rightArrow from './logo/right.png';
-export const logo = { mainLogo, welder, leftArrow, rightArrow };
